4b67aa3823aef430453597ff13f56c8ba4ee7ac0
Eric Yang <lotayou@pku.edu.cn>
2019-04-14 03:11:27 +0800

    20190414: Add training script.

---
+++ models/base_model.py
@@ -0,0 +1,104 @@
+import os
+import torch
+from collections import OrderedDict
+from . import networks
+
+class BaseModel():
+    def name(self):
+        return 'BaseModel'
+
+    def initialize(self, opt):
+        self.opt = opt
+        self.isTrain = opt.phase == 'train'
+        self.device = torch.device('cuda' if torch.cuda.is_available() else torch.device('cpu'))
+        self.save_dir = os.path.join(opt.checkpoints_dir, opt.name)
+        if not os.path.isdir(self.save_dir):
+            os.makedirs(self.save_dir)
+        torch.backends.cudnn.benchmark = True
+        
+    def setup(self, opt):
+        if self.isTrain:
+            self.schedulers = [networks.get_scheduler(optimizer, opt) for optimizer in self.optimizers]
+
+        if not self.isTrain or opt.continue_train:
+            self.load_networks(opt.load_epoch)
+        self.print_networks(opt.verbose)
+
+    def set_input(self, input):
+        self.input = input
+
+    def forward(self):
+        pass
+
+    def is_train(self):
+        return True
+
+    def set_requires_grad(self, net, requires_grad=False):
+        if net is not None:
+            for param in net.parameters():
+                param.requires_grad = requires_grad  # to avoid computation
+
+    # used in test time, wrapping `forward` in no_grad() so we don't save
+    # intermediate steps for backprop
+    def test(self):
+        with torch.no_grad():
+            self.forward()
+
+    # get image paths
+    def get_image_paths(self):
+        return self.image_paths
+
+    def optimize_parameters(self):
+        pass
+
+    # update learning rate (called once every epoch)
+    def update_learning_rate(self, metrics):
+        for scheduler in self.schedulers:
+            if self.opt.lr_policy == 'plateau':
+                scheduler.step(metrics=metrics)
+            else:
+                scheduler.step()
+        lr = self.optimizers[0].param_groups[0]['lr']
+        print('learning rate = %.7f' % lr)
+
+    # make models eval mode during test time
+    def eval(self):
+        for name in self.model_names:
+            if isinstance(name, str):
+                net = getattr(self, name)
+                net.eval()
+
+    # save models to the disk
+    def save_networks(self, epoch):
+        for name in self.model_names:
+            if isinstance(name, str):
+                save_filename = '%s_net_%s.pth' % (epoch, name)
+                save_path = os.path.join(self.save_dir, save_filename)
+                net = getattr(self, name)
+                torch.save(net.state_dict(), save_path)
+        print('Checkpoints saved at epoch %d' % epoch)
+
+    # load models from the disk
+    def load_networks(self, epoch):
+        for name in self.model_names:
+            if isinstance(name, str):
+                load_filename = '%s_net_%s.pth' % (epoch, name)
+                load_path = os.path.join(self.save_dir, load_filename)
+                state_dict = torch.load(load_path)
+                net = getattr(self, name)
+                net.load_state_dict(state_dict)
+        print('Checkpoints loaded. Resume training from epoch %d' % epoch)
+
+    # print network information
+    def print_networks(self, verbose):
+        print('---------- Networks initialized -------------')
+        for name in self.model_names:
+            if isinstance(name, str):
+                net = getattr(self, name)
+                num_params = 0
+                for param in net.parameters():
+                    num_params += param.numel()
+                if verbose:
+                    print(net)
+                print('[Network %s] Total number of parameters : %.3f M' % (name, num_params / 1e6))
+        print('-----------------------------------------------')
